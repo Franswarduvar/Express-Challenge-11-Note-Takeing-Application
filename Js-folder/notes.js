44c814529159f228d5061d85eaf18a85a911c442
B.O.A Addington <Franswarduvar>
2022-08-19 15:15:37 -0700

    commiting functening code besides the delete function

--- Js-folder/notes.js
+++ Js-folder/notes.js
@@ -2,8 +2,13 @@ const path = require('path');
 const fs = require('fs');
 
 function createNote(body, notes){
-
+notes.push(body);
+fs.writeFileSync(path.join(__dirname, '../db/db.json'),JSON.stringify({
+    notes:notes
+},null,2));
+return body 
 }
+
 module.exports = {
     createNote
 }
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -7,4 +7,8 @@ const { get } = require('./htmlroutes');
 router.get('/notes', (req,res)=>{
     res.json(notes);
 });
+router.post('/notes', (req,res)=>{
+    req.body.id = uuidv4();
+    res.json(createNote(req.body,notes));
+});
 module.exports = router;
